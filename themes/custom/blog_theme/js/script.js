ec00b07aab03e85d4d4c19ec5272c9415d6da225
sharayurajput1998@gmail.com <sharayurajput1998@gmail.com>
2021-12-01 10:46:08 +0530

    update code

--- themes/custom/blog_theme/js/script.js
+++ themes/custom/blog_theme/js/script.js
@@ -1,17 +1,32 @@
-$(document).ready(function () {
-    $(".navbar").click(function () {
-        var x = $(window).width();
-        if (x < 768) {
-            $(".minimize").toggle();
+const toggle = document.getElementById("toggle");
+toggle.onclick = function () {
+    toggle.classList.toggle("active");
+    document.body.classList.toggle('dark_theme');
+}
+  
+
+  jQuery(document).ready(function () {
+    jQuery(".navbar").click(function () {
+        var x = jQuery(window).width();
+        if (x < 850) {
+            jQuery(".minimize").toggle();
         }
     });
 });
 
-$(document).resize(function () {
-    $(".navbar").click(function () {
-        var x = $(window).width();
-        if (x < 768) {
-            $(".minimize").toggle();
+jQuery(document).resize(function () {
+    jQuery(".navbar").click(function () {
+        var x = jQuery(window).width();
+        if (x < 850) {
+            jQuery(".minimize").toggle();
         }
     });
 });
+
+
+jQuery(document).ready(function () {
+    jQuery(".post_icon").click(function () {
+        jQuery(this).siblings('.index_link').toggle();
+    });
+});
+
